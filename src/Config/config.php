58f71332377651461a289af3c2cddbd6dd53e8e7
Eduardo Marcos <edujugon@gmail.com>
2016-09-13 16:10:17 +0200

    update

--- src/Config/config.php
+++ src/Config/config.php
@@ -11,8 +11,8 @@ return [
   ],
   'apn' => [
       'certificate' => __DIR__ . '/iosCertificates/ck.pem',
-      'passPhrase' => '123456',
-      'passFile' => __DIR__ . '/iosCertificates/yourKey.pem',
+      'passPhrase' => '123456', //Optional
+      'passFile' => __DIR__ . '/iosCertificates/yourKey.pem', //Optional
       'dry_run' => false
   ]
 ];
