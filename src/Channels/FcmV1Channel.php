369f6eb2614cab3a5eabdba3ae0d811079b3ae1b
Manuel Rodríguez <mrodriguez@whiteguru.com>
2024-09-23 09:09:27 +0200

    Fix Laravel notification payload generation

--- src/Channels/FcmV1Channel.php
+++ src/Channels/FcmV1Channel.php
@@ -2,6 +2,8 @@
 
 namespace Edujugon\PushNotification\Channels;
 
+use Edujugon\PushNotification\Messages\PushMessage;
+
 class FcmV1Channel extends GcmChannel
 {
     /**
@@ -10,5 +12,60 @@ class FcmV1Channel extends GcmChannel
     protected function pushServiceName()
     {
         return 'fcmv1';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function buildData(PushMessage $message)
+    {
+        $data = [];
+
+        if ($message->title != null || $message->body != null) {
+            $data = [
+                'notification' => [
+                    'title' => $message->title,
+                    'body' => $message->body,
+                ],
+            ];
+        }
+
+        if (
+            $message->icon ||
+            $message->color ||
+            $message->sound ||
+            $message->click_action ||
+            $message->badge
+        ) {
+            $data['android'] = [
+                'notification' => []
+            ];
+
+            if (! empty($message->icon)) {
+                $data['android']['notification']['icon'] = $message->icon;
+            }
+
+            if (! empty($message->color)) {
+                $data['android']['notification']['color'] = $message->color;
+            }
+
+            if (! empty($message->sound)) {
+                $data['android']['notification']['sound'] = $message->sound;
+            }
+
+            if (! empty($message->click_action)) {
+                $data['android']['notification']['click_action'] = $message->click_action;
+            }
+
+            if (! empty($message->badge)) {
+                $data['android']['notification']['notification_count'] = $message->badge;
+            }
+        }
+
+        if (! empty($message->extra)) {
+            $data['data'] = $message->extra;
+        }
+
+        return $data;
     }
 }
